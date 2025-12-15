d6fbe118c43ce4e576280dd07ec55ef3f866ea49
vantoan252003 <37nguyenvantoan@gmail.com>
2025-12-15 13:42:18 +0700

    dang nhap bang fb

--- android/app/src/main/kotlin/com/example/student_ecommerce/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/student_ecommerce/MainActivity.kt
@@ -1,5 +1,33 @@
 package com.toan.student_ecommerce
 
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
+import java.security.MessageDigest
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        printHashKey()
+    }
+
+    private fun printHashKey() {
+        try {
+            val info = packageManager.getPackageInfo(
+                packageName,
+                PackageManager.GET_SIGNATURES
+            )
+            for (signature in info.signatures) {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val hashKey = Base64.encodeToString(md.digest(), Base64.DEFAULT)
+                Log.d("Facebook KeyHash", hashKey)
+                println("🔑 Facebook Key Hash: $hashKey")
+            }
+        } catch (e: Exception) {
+            Log.e("Facebook KeyHash", "Error: ${e.message}")
+        }
+    }
+}
